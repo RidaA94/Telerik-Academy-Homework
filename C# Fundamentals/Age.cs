e905f2035cd0025d1002bc093c96c82e10e3c760
RidaA94 <ridaamayre@gmail.com>
2016-10-12 15:09:17 +0300

    Homework C# fundamentals

---
+++ C# Fundamentals/Age.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        DateTime inputDate = DateTime.ParseExact(input, "MM.dd.yyyy", null);
+
+        int currentAge = GetAge(inputDate);
+        int ageAfter10years = currentAge + 10;
+
+        Console.WriteLine(currentAge);
+        Console.WriteLine(ageAfter10years);
+    }
+
+    private static int GetAge(DateTime date)
+    {
+        DateTime currentDate = DateTime.Now;
+
+        int age = currentDate.Year - date.Year;
+
+        if (date.Month > currentDate.Month)
+        {
+            age--;
+        }
+        else if (date.Month == currentDate.Month)
+        {
+            if (date.Day > currentDate.Day)
+            {
+                age--;
+            }
+        }
+
+        return age;
+    }
+}
---
+++ C# Fundamentals/SquareRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class HelloC
+{
+    static void Main()
+    {
+        int num = 12345;
+        Console.WriteLine(Math.Sqrt(num));
+    }
+}
