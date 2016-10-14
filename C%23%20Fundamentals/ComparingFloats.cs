a13bd1f2cf1e617fa0ba2b2841c2d1f0d947185b
RidaA94 <ridaamayre@gmail.com>
2016-10-14 14:34:34 +0300

    Add files via upload

---
+++ C%23%20Fundamentals/ComparingFloats.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace _13.ComparingFloats
+{
+    class CompareFloats
+    {
+        static void Main(string[] args)
+        {
+
+
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double difference = Math.Abs(a - b);
+            double eps = 0.000001;
+            bool isEqual = true;
+
+            if (difference >= eps)
+            {
+                isEqual = false;
+            }
+
+            string output = "false";
+
+            if (isEqual)
+            {
+                output = "true";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
